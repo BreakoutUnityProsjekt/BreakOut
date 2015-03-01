e8215e3c9a36c1786f9dbfb30dedb764a31f5d79
Racksay <edvin.klaebo@gmail.com>
2015-03-01 21:04:48 +0100

    First release

---
+++ Assets/scripts/Bricks.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bricks : MonoBehaviour {
+
+	public GameObject brickParticle;
+	private GameObject particle = null;
+
+
+	void OnCollisionEnter(Collision other){
+		particle = (GameObject) Instantiate (brickParticle, transform.position, Quaternion.identity);
+		if (this.name == "redBrick") {
+			GameManager.instance.score += 4;
+		}
+		if (this.name == "blueBrick") {
+			GameManager.instance.score += 9;		
+		}
+		if(this.name == "greenBricks"){
+			GameManager.instance.score += 14;
+		}
+		GameManager.instance.OnBlockDestroyed ();
+		Destroy (gameObject);
+		Destroy (particle, 5f);
+	}
+
+}
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public int life = 3;
+	public int blocks = 100;
+	public int score = 0;
+	public int restartDelay = 3;
+	public Text livesText;
+	public Text scoreText;
+	public GameObject gameOver;
+	public GameObject youWon;
+	public GameObject bricksPrefab;
+	public GameObject paddle;
+	public GameObject clonePaddle;
+	public GameObject deathParticles;
+	public static GameManager instance;
+	public Rigidbody ballRigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		Setup ();
+	}
+	public void Setup(){
+		clonePaddle = Instantiate (paddle, transform.position, Quaternion.identity) as GameObject;
+	}
+	void Restart(){
+		Application.LoadLevel (1);
+	}
+	
+	void WinOrLoseCheck(){
+		if (blocks <= 0) {
+			youWon.SetActive(true);
+			Invoke("Restart", restartDelay);
+		}
+		if (life <= 0) {
+			gameOver.SetActive(true);
+			Invoke("Restart", restartDelay);
+		}
+	}
+
+	
+	public void LoseLife(){
+		life--;
+		livesText.text = "Lives: " + life;
+		Destroy (clonePaddle);
+		Controller.instance.DestroyBall ();
+		Invoke ("SetupPaddle", 1);
+		WinOrLoseCheck ();
+	}
+	
+	void SetupPaddle(){
+		clonePaddle = Instantiate (paddle, transform.position, Quaternion.identity) as GameObject;
+	}
+	
+	public void OnBlockDestroyed(){
+		blocks--;
+		score++; 
+		scoreText.text = "Score: " + score;
+		WinOrLoseCheck ();
+	}
+	
+}
