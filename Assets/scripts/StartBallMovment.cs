cb64310caa0d8fcb293c205190ac38facbc53042
dannemeinecke <dannemeinecke@gmail.com>
2015-03-06 10:46:36 +0100

    Endringer på statskjerm

---
+++ Assets/scripts/StartBallMovment.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartBallMovment : MonoBehaviour{
+
+	public Rigidbody ballRidigBody; 
+
+	// Use this for initialization
+	void Start () {
+		ballRidigBody.AddForce(new Vector3(10000f,1000f,0f));
+	}	
+}
